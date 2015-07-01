4c52111a577f5535072c569fc87d8b72f3c80be0
Brian Jacobel <bjacobel@gmail.com>
2015-07-01 12:49:03 -0400

    Added overfeat, close to doing classification

--- goggles.go
+++ goggles.go
@@ -1,10 +1,140 @@
 package main
 
 import (
-	"C"
-	"github.com/bjacobel/goggles/twitter"
+	"github.com/chimeracoder/anaconda"
+	"github.com/davecgh/go-spew/spew"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os/exec"
+	"os"
+	"strings"
+	"net/http"
+	"io"
+	"log"
+	"path"
+	"runtime"
+	"net/url"
 )
 
+var spw = spew.NewDefaultConfig()
+
+func init() {
+	// Add some configuration to our JSON logger
+	spw = &spew.ConfigState{Indent: "\t", MaxDepth: 5}
+}
+
+type Config struct {
+	ConsumerKey       string `yaml:"consumer_key"`
+	ConsumerSecret    string `yaml:"consumer_secret"`
+	AccessToken       string `yaml:"access_token"`
+	AccessTokenSecret string `yaml:"access_token_secret"`
+}
+
+func Parse(path string) Config {
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
+
 func main() {
-	twitter.Stream()
+	config := Parse("secrets.yml")
+
+	// can't stop giggling every time I see "anaconda"
+	// thank you, sir mix-a-lot
+	anaconda.SetConsumerKey(config.ConsumerKey)
+	anaconda.SetConsumerSecret(config.ConsumerSecret)
+	twitter := anaconda.NewTwitterApi(config.AccessToken, config.AccessTokenSecret)
+
+	v := url.Values{}
+	v.Set("language", "en")
+
+	stream := twitter.PublicStreamSample(v)
+
+	for {
+		select {
+		case <-stream.Quit:
+			log.Fatal("Stream terminated, wrapping up and quitting...")
+			break
+		case tweet := <-stream.C:
+			go handleTweet(tweet)
+		}
+	}
+}
+
+func handleTweet(t interface{}) {
+	// Type assertion to anaconda.Tweet from interface{}
+	if tweet, ok := t.(anaconda.Tweet); ok {
+		if hasMedia(tweet) {
+			identify(getMediaURL(tweet))
+		}
+	} else {
+		log.Fatal("Tried to handle a tweet that was not a tweet (?)")
+	}
+}
+
+func hasMedia(tweet anaconda.Tweet) bool {
+	return tweet.Entities.Media != nil
+}
+
+func getMediaURL(tweet anaconda.Tweet) string {
+	return tweet.Entities.Media[0].Media_url_https
+}
+
+func identify(url string) {
+	// download it, save to tmp
+	fileName := download(url)
+
+	// process it
+	_, filename, _, _ := runtime.Caller(1)
+
+	out, err := exec.Command(path.Dir(filename) + "/overfeat/bin/linux_32/overfeat -n 1" + fileName).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println(string(out))
+	}
+
+	// delete from /tmp
+}
+
+func download(url string) string {
+	tokens := strings.Split(url, "/")
+	fileName := "tmp/" + tokens[len(tokens)-1]
+
+	output, err := os.Create(fileName)
+
+	if err != nil {
+		log.Fatal("Error while creating", fileName, "-", err)
+	}
+
+	defer output.Close()
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal("Error while downloading", url, "-", err)
+	}
+
+	defer response.Body.Close()
+
+	_, er := io.Copy(output, response.Body)
+
+	if er != nil {
+		log.Fatal("Error while saving", url, "-", er)
+	}
+
+	log.Println(fileName)
+
+	return fileName
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@ Goggles is a Go interface for image classification.
 
 See `twitter.go` for an example of using Goggles to classify images from the Twitter Streaming API.
 
+####Requirements
+- `imagemagick`
+- `libopenblas-dev`
