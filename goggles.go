e79d7c2fb66286c6d6ac596c92dc82a367aa7324
Brian Jacobel <bjacobel@gmail.com>
2015-08-07 17:38:43 -0500

    Posting to Twitter with dumb quips. Things are sync and slow, still

--- goggles.go
+++ goggles.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -17,10 +18,20 @@ import (
 )
 
 var spw = spew.NewDefaultConfig()
+var twitter *anaconda.TwitterApi
 
 func init() {
 	// Add some configuration to our JSON logger
 	spw = &spew.ConfigState{Indent: "\t", MaxDepth: 5}
+
+	// Parse a YAML file to get secret configuration
+	config := Parse("secrets.yml")
+
+	// can't stop giggling every time I see "anaconda"
+	// thank you, sir mix-a-lot
+	anaconda.SetConsumerKey(config.ConsumerKey)
+	anaconda.SetConsumerSecret(config.ConsumerSecret)
+	twitter = anaconda.NewTwitterApi(config.AccessToken, config.AccessTokenSecret)
 }
 
 type Config struct {
@@ -47,14 +58,6 @@ func Parse(path string) Config {
 }
 
 func main() {
-	config := Parse("secrets.yml")
-
-	// can't stop giggling every time I see "anaconda"
-	// thank you, sir mix-a-lot
-	anaconda.SetConsumerKey(config.ConsumerKey)
-	anaconda.SetConsumerSecret(config.ConsumerSecret)
-	twitter := anaconda.NewTwitterApi(config.AccessToken, config.AccessTokenSecret)
-
 	v := url.Values{}
 	v.Set("language", "en")
 
@@ -82,7 +85,7 @@ func handleTweet(t interface{}) {
 			}
 		}
 	} else {
-		log.Fatal("Tried to handle a tweet that was not a tweet (?)")
+		log.Println("Tried to handle a tweet that was not a tweet (?)")
 	}
 }
 
@@ -99,10 +102,10 @@ func identify(url string) ([]string, float64) {
 	fileName := download(url)
 
 	// process it
-	out, err := exec.Command("./OverFeat/bin/macos/overfeat", "-n 1", fileName).CombinedOutput()
+	out, err := exec.Command("./OverFeat/src/overfeat", "-n 1", fileName).CombinedOutput()
 
 	if err != nil {
-		log.Fatal(out, err)
+		log.Println(out, err)
 	}
 
 	split := strings.Split(string(out), " ")
@@ -121,7 +124,7 @@ func download(url string) string {
 	output, err := os.Create(fileName)
 
 	if err != nil {
-		log.Fatal("Error while creating", fileName, "-", err)
+		log.Println("Error while creating", fileName, " - ", err)
 	}
 
 	defer output.Close()
@@ -129,7 +132,7 @@ func download(url string) string {
 	response, err := http.Get(url)
 
 	if err != nil {
-		log.Fatal("Error while downloading", url, "-", err)
+		log.Println("Error while downloading", url, " - ", err)
 	}
 
 	defer response.Body.Close()
@@ -137,18 +140,94 @@ func download(url string) string {
 	_, er := io.Copy(output, response.Body)
 
 	if er != nil {
-		log.Fatal("Error while saving", url, "-", er)
+		log.Println("Error while saving", url, "-", er)
 	}
 
 	return fileName
 }
 
 func respond(tweet anaconda.Tweet, classification []string) {
-	msg := fmt.Sprintf("That's a %s!", classification[0])
+	msg := fmt.Sprintf(
+		"%s https://twitter.com/%s/status/%s",
+		message(classification[0]),
+		tweet.User.IdStr,
+		tweet.IdStr,
+	)
 
-	if len(classification) > 1 {
-		msg = fmt.Sprintf("%s Or maybe a %s?", msg, classification[1])
+	reply, errRpl := twitter.PostTweet(msg, url.Values{})
+
+	if errRpl != nil {
+		log.Printf("Error responding: ", errRpl)
+		return
+	} else {
+		log.Printf("Tweeted; link: https://twitter.com/%s/status/%d\n", reply.User.ScreenName, reply.Id)
 	}
+}
 
-	log.Println(msg)
+type Quip struct {
+	Message    string
+	Articleize bool
+}
+
+// pairs of messages and whether the noun must be articleized to fit them
+var quipOptions = []Quip{
+	Quip{"Look what I found! I think it's %s.", true},
+	Quip{"This looks like %s!", true},
+	Quip{"What do we have here? It looks like %s.", true},
+	Quip{"I found %s!", true},
+	Quip{"I've never seen %s like this before!", true},
+	Quip{"Wow, I almost didn't recognize this as %s!", true},
+	Quip{"Cool! It's %s!", true},
+	Quip{"I could be wrong, but this looks like %s to me.", true},
+	Quip{"Bet you didn't think I'd recognize this ;) It's %s!", true},
+	Quip{"Wow, %s! Didn't think I'd see one of those today :)", true},
+	Quip{"I found %s! Now I've seen everything :)", true},
+	Quip{"Look, %s!", true},
+	Quip{"Does this look like %s to you? I think it is, but I'm never sure!", true},
+	Quip{"Awesome, %s! Thanks for sharing ;)", true},
+	Quip{"Coolest %s I've seen today!", false},
+	Quip{"Love this %s!", false},
+	Quip{"Wow, look at this %s! Super cool.", false},
+	Quip{"This looks just like the %s I have!", false},
+	Quip{"This might be my favorite %s I've ever seen.", false}
+	Quip{"I want %s like this :(((", true},
+	Quip{"Wonder where they got this sweet %s?", false},
+	Quip{"Whoa, %s? Now I've seen everything.", true},
+}
+
+func message(noun string) string {
+	choice := quipOptions[rand.Intn(len(quipOptions))]
+
+	if choice.Articleize {
+		return fmt.Sprintf(choice.Message, articleize(noun))
+	} else {
+		return fmt.Sprintf(choice.Message, noun)
+	}
+}
+
+func isVowel(letter string) bool {
+	// @TODO: This is super quick n' dirty. Obviously English has lots of corner cases
+	// not covered by this, we want to test for vowel "sounds" not just vowels
+	switch letter {
+	case "a":
+		return true
+	case "e":
+		return true
+	case "i":
+		return true
+	case "o":
+		return true
+	case "u":
+		return true
+	default:
+		return false
+	}
+}
+
+func articleize(noun string) string {
+	if isVowel(noun[:1]) {
+		return fmt.Sprintf("an %s", noun)
+	} else {
+		return fmt.Sprintf("a %s", noun)
+	}
 }
--- readme.md
+++ readme.md
@@ -7,4 +7,6 @@ See `twitter.go` for an example of using Goggles to classify images from the Twi
 
 ####Requirements
 - `imagemagick`
-- `libopenblas-dev`
+- OverFeat weight files (run `python OverFeat/download_weights.py`)
+- OverFeat compiled (`git submodule init && cd OverFeat/src && make`)
+    - (In order to actually get this to compile you will have to do some mucking. It's not easy. Sorry!)
